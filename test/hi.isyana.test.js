30649a41732deb6355e89cd8b8909accdbff2d0d
Abdul Fattah Ikhsan <ikhsannetwork@gmail.com>
2018-01-28 23:37:00 +0700

    add test

--- test/hi.isyana.test.js
+++ test/hi.isyana.test.js
@@ -1,1 +1,10 @@
+"use strict";
 // Just kidding #lol
+exports.__esModule = true;
+var chai_1 = require("chai");
+require("mocha");
+describe('Hi isyana', function () {
+    it('should said hi too', function () {
+        chai_1.expect(true).to.be["true"];
+    });
+});
--- test/hi.isyana.test.ts
+++ test/hi.isyana.test.ts
@@ -1,1 +1,12 @@
 // Just kidding #lol
+
+import { expect } from 'chai';
+import 'mocha';
+
+describe('Hi isyana', () => {
+
+  it('should said hi too', () => {
+    expect(true).to.be.true;
+  });
+
+});
